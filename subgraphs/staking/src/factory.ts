f3409ce118f5a604e8cd1b8cc94fe3670a162fcb
Vartan Babayan <vardbabayan.work@gmail.com>
2024-05-08 00:18:10 +0300

    [Fix] indexing factory events for each type of pool

--- subgraphs/staking/src/factory.ts
+++ subgraphs/staking/src/factory.ts
@@ -1,16 +1,53 @@
 import { Factory, Pool, Token } from "../generated/schema";
+
 import { 
-  CreateStakingPool as NewStakingPoolEvent 
+  CreateStakingPool as LockUpStakingPoolEvent 
 } from "../generated/ERC20LockUpFactory/ERC20LockUpStakingFactory"
+
+import {
+  CreateStakingPool as NoLockUpStakingPoolEvent
+} from "../generated/ERC20NoLockUpFactory/ERC20NoLockUpStakingFactory"
+
+import {
+  CreateStakingPool as PenaltyFeeStakingPoolEvent
+} from "../generated/ERC20NoLockUpFactory/ERC20NoLockUpStakingFactory"
+
 import { ERC20LockUpStakingPool as StakingPoolTemplate } from "../generated/templates";
 import { BigInt, log } from "@graphprotocol/graph-ts";
 
-export function handleStakingPoolCreation(event: NewStakingPoolEvent): void {
+export function handleLockUpStakingPoolCreation(event: LockUpStakingPoolEvent): void {
   const factoryAddress = event.address.toHex();
   const poolAddress = event.params.stakingAddress.toString();
-  // const tokenAddress = event.params.stakeToken.toString();
-  // let pool = Pool.load(poolAddress)!; ???
-  // let token = Token.load(tokenAddress)!; ???
+  const tokenAddress = event.params.stakeToken.toString();
+
+  let factory = Factory.load(factoryAddress);
+  if (factory === null) {
+    factory = new Factory(factoryAddress);
+    factory.totalPools = BigInt.fromI32(0);
+    factory.save();
+  }
+  factory.totalPools = factory.totalPools.plus(BigInt.fromI32(1));
+  factory.poolAddress.push(poolAddress);
+  factory.save();
+
+  let pool = new Pool(poolAddress);
+  pool.stakeToken = tokenAddress;
+  pool.startTime = event.block.timestamp;
+  pool.save();
+  
+  let token = Token.load(tokenAddress);
+  if (!token) {
+    token = new Token(tokenAddress);
+    token.save();
+  }
+
+  StakingPoolTemplate.create(event.params.stakingAddress);
+}
+
+export function handleNoLockUpStakingPoolCreation(event: NoLockUpStakingPoolEvent): void {
+  const factoryAddress = event.address.toHex();
+  const poolAddress = event.params.stakingAddress.toString();
+  const tokenAddress = event.params.stakeToken.toString();
 
   let factory = Factory.load(factoryAddress);
   if (!factory) {
@@ -18,16 +55,49 @@ export function handleStakingPoolCreation(event: NewStakingPoolEvent): void {
     factory.totalPools = BigInt.fromI32(0);
     factory.save();
   }
-
   factory.totalPools = factory.totalPools.plus(BigInt.fromI32(1));
   factory.poolAddress.push(poolAddress);
-
-  // token.save()
-  
   factory.save();
 
-  //TODO Handle pool creation here based on the factoty type (nolockup/lockup/penalty)
-  //pool.save()
-  //StakingPoolTemplate.create(event.params.stakingAddress);
-  //log.info("Pool initialized: {}", [pool.id]);
+  let pool = new Pool(poolAddress);
+  pool.stakeToken = tokenAddress;
+  pool.startTime = event.block.timestamp;
+  pool.save();
+  
+  let token = Token.load(tokenAddress);
+  if (!token) {
+    token = new Token(tokenAddress);
+    token.save();
+  }
+
+  StakingPoolTemplate.create(event.params.stakingAddress);
+}
+
+export function handlePenaltyFeePoolCreation(event: PenaltyFeeStakingPoolEvent): void {
+  const factoryAddress = event.address.toHex();
+  const poolAddress = event.params.stakingAddress.toString();
+  const tokenAddress = event.params.stakeToken.toString();
+
+  let factory = Factory.load(factoryAddress);
+  if (!factory) {
+    factory = new Factory(factoryAddress);
+    factory.totalPools = BigInt.fromI32(0);
+    factory.save();
+  }
+  factory.totalPools = factory.totalPools.plus(BigInt.fromI32(1));
+  factory.poolAddress.push(poolAddress);
+  factory.save();
+
+  let pool = new Pool(poolAddress);
+  pool.stakeToken = tokenAddress;
+  pool.startTime = event.block.timestamp;
+  pool.save();
+  
+  let token = Token.load(tokenAddress);
+  if (!token) {
+    token = new Token(tokenAddress);
+    token.save();
+  }
+
+  StakingPoolTemplate.create(event.params.stakingAddress);
 }
