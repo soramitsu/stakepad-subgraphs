27bb22e1826d4d83f11d5ee25a11dbaa074aeba5
Vartan Babayan <vardbabayan.work@gmail.com>
2024-06-07 13:45:30 +0300

    Added handlers for ERC721 factory, Adjusted nftoken and manifest for ERC721

---
+++ subgraphs/staking/src/nft_factory.ts
@@ -0,0 +1,93 @@
+import { Pool, Request } from "../generated/schema";
+
+import {
+  StakingPoolDeployed,
+  RequestSubmitted as LockUpPoolRequestSubmitted,
+  RequestStatusChanged
+} from "../generated/ERC721LockUpFactory/ERC721LockUpStakingFactory"
+
+import {
+  RequestSubmitted as PenaltyPoolRequestSubmitted
+} from "../generated/ERC721PenaltyFeeFactory/ERC721PenaltyFeeStakingFactory"
+
+
+import { ERC721LockUpStakingPool as StakingPoolTemplate } from "../generated/templates";
+import { BigInt, Address } from "@graphprotocol/graph-ts";
+import { fetchToken } from "./utils/token";
+import { getOrCreateFactory } from "./utils/factory";
+
+export function handleLockUpPoolRequestSubmitted(event: LockUpPoolRequestSubmitted): void {
+  const requestId = event.address.toHex() + "-" + event.params.id.toString();
+
+  let request = new Request(requestId);
+  request.deployer = event.params.deployer.toHex();
+  request.requestStatus = BigInt.fromI32(1);
+  request.stakeToken = event.params.data.stakeToken.toHex();
+  request.rewardToken = event.params.data.rewardToken.toHex();
+  request.poolStartTime = event.params.data.poolStartTime;
+  request.poolEndTime = event.params.data.poolEndTime;
+  request.rewardPerSecond = event.params.data.rewardPerSecond;
+  request.unstakeLockUpTime = event.params.data.unstakeLockUpTime;
+  request.claimLockUpTime = event.params.data.claimLockUpTime;
+  request.penaltyPeriod = BigInt.fromI32(0);
+  request.save();
+}
+
+export function handlePenaltyPoolRequestSubmitted(event: PenaltyPoolRequestSubmitted): void {
+  const requestId = event.address.toHex() + "-" + event.params.id.toString();
+
+  let request = new Request(requestId);
+  request.deployer = event.params.deployer.toHex();
+  request.requestStatus = BigInt.fromI32(1);
+  request.stakeToken = event.params.data.stakeToken.toHex();
+  request.rewardToken = event.params.data.rewardToken.toHex();
+  request.poolStartTime = event.params.data.poolStartTime;
+  request.poolEndTime = event.params.data.poolEndTime;
+  request.rewardPerSecond = event.params.data.rewardPerSecond;
+  request.unstakeLockUpTime = BigInt.fromI32(0);
+  request.claimLockUpTime = BigInt.fromI32(0);
+  request.penaltyPeriod = event.params.data.penaltyPeriod;
+  request.save();
+}
+
+export function handlePoolDeployment(event: StakingPoolDeployed): void {
+  const poolAddress = event.params.stakingAddress.toHex();
+  const requestId = event.address.toHex() + "-" + event.params.id.toString();
+
+  let factory = getOrCreateFactory(event.address);
+  factory.totalPools = factory.totalPools.plus(BigInt.fromI32(1));
+  factory.poolAddress.push(poolAddress);
+  factory.save();
+
+  let request = Request.load(requestId)!;
+  request.requestStatus = BigInt.fromI32(4);
+  request.save();
+
+  let stakeToken = fetchToken(Address.fromString(request.stakeToken));
+  let rewardToken = fetchToken(Address.fromString(request.rewardToken));
+
+  let pool = new Pool(poolAddress);
+  pool.stakeToken = stakeToken.id;
+  pool.rewardToken = rewardToken.id;
+  pool.startTime = request.poolStartTime;
+  pool.endTime = request.poolEndTime;
+  pool.unstakeLockUpTime = request.unstakeLockUpTime;
+  pool.claimLockUpTime = request.claimLockUpTime;
+  pool.penaltyPeriod = request.penaltyPeriod;
+  pool.rewardTokenPerSecond = request.rewardPerSecond;
+  pool.totalStaked = BigInt.fromI32(0);
+  pool.totalClaimed = BigInt.fromI32(0);
+  pool.accRewardPerShare = BigInt.fromI32(0);
+  pool.lastRewardTimestamp = request.poolStartTime;
+  pool.owner = request.deployer;
+  pool.save();
+
+  StakingPoolTemplate.create(event.params.stakingAddress);
+}
+
+export function handlePoolStatusChanged(event: RequestStatusChanged): void {
+  const requestId = event.address.toHex() + "-" + event.params.id.toString();
+  let request = Request.load(requestId)!;
+  request.requestStatus = BigInt.fromI32(event.params.status);
+  request.save();
+}
--- subgraphs/staking/src/utils/token.ts
+++ subgraphs/staking/src/utils/token.ts
@@ -1,6 +1,7 @@
 import { BigInt, Address } from "@graphprotocol/graph-ts";
-import { Token } from "../../generated/schema";
+import { Token, NFToken } from "../../generated/schema";
 import { ERC20 } from "../../generated/ERC20LockUpFactory/ERC20"
+import { ERC721 } from "../../generated/ERC721LockUpFactory/ERC721"
 
 export function fetchToken(tokenAddress: Address): Token {
     let token = Token.load(tokenAddress.toHexString());
@@ -11,7 +12,7 @@ export function fetchToken(tokenAddress: Address): Token {
       let tryName = tokenContract.try_name();
       let trySymbol = tokenContract.try_symbol();
       let tryDecimals = tokenContract.try_decimals();
-      
+
       token.name = tryName.reverted ? "" : tryName.value;
       token.symbol = trySymbol.reverted ? "" : trySymbol.value;
       token.decimals = tryDecimals.reverted ? BigInt.fromI32(0) : BigInt.fromI32(tryDecimals.value);
@@ -19,4 +20,19 @@ export function fetchToken(tokenAddress: Address): Token {
     }
 
     return token as Token;
+}
+
+export function fetchNFToken(tokenAddress: Address): NFToken {
+  let token = NFToken.load(tokenAddress.toHexString());
+
+  if (token == null) {
+    token = new NFToken(tokenAddress.toHexString());
+    let tokenContract = ERC721.bind(tokenAddress);
+    token.tokenId = "";
+    token.owner = "";
+    token.pool = "";
+    token.save();
   }
+
+  return token as NFToken;
+}
